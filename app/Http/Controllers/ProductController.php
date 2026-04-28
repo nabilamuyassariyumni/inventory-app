c07a527b5ad65145eedce7759e7c9a00f01d08ba
Nabila Muyassari Yumni <nabilamuyassariyumni@gmail.com>
2026-04-28 12:22:02 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,10 +7,49 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index() 
-    { 
-        // Mengambil produk beserta kategori terkait 
+    public function index()
+    {
         $products = Product::with('category')->latest()->paginate(10);
         return view('products.index', compact('products'));
+    }
+
+    //INSERT
+    public function insert()
+    {
+        Product::create([
+            'name' => 'Produk Baru',
+            'category_id' => 1,
+            'price' => 10000,
+            'stock' => 10,
+            'description' => 'Produk otomatis',
+            'status' => 'tersedia'
+        ]);
+
+        return redirect('/products')->with('success', 'Data berhasil ditambahkan');
+    }
+
+    //UPDATE
+    public function update($id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->update([
+            'name' => 'Produk Update',
+            'price' => 20000,
+            'stock' => 5,
+            'description' => 'Produk yang diupdate',
+            'status' => 'habis'
+        ]);
+
+        return redirect('/products')->with('success', 'Data berhasil diupdate');
+    }
+
+    //DELETE
+    public function delete($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return redirect('/products')->with('success', 'Data berhasil dihapus');
     }
 }
