172598fe89a52f079306468c69c317332ee61f3f
Nabila Muyassari Yumni <nabilamuyassariyumni@gmail.com>
2026-04-28 12:22:08 +0700

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <h1>Daftar Barang Inventaris </h1>
+<a href="/insert" class="btn btn-primary mb-3">Tambah Data</a>
 <table class="table table-striped">
     <thead>
         <tr>
@@ -8,6 +9,9 @@
             <th>Kategori </th>
             <th>Harga </th>
             <th>Stok </th>
+            <th>Deskripsi</th>
+            <th>Status</th>
+            <th>Aksi</th>
         </tr>
     </thead>
     <tbody>
@@ -17,6 +21,12 @@
             <td>{{ $p->category->name }} </td>
             <td>Rp {{ number_format($p->price) }} </td>
             <td>{{ $p->stock }} </td>
+            <td>{{ $p->description }}</td>
+            <td>{{ $p->status }}</td>
+            <td>
+                <a href="/products/update/{{ $p->id }}" class="btn btn-warning btn-sm">Update</a>
+                <a href="/products/delete/{{ $p->id }}" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin menghapus data?')">Delete</a>
+            </td>
         </tr>
         @endforeach
     </tbody>
