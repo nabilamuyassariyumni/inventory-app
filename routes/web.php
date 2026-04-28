ae994746448f84ec23b6eb973255d4dd02571022
Nabila Muyassari Yumni <nabilamuyassariyumni@gmail.com>
2026-04-28 12:22:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', function () {
 });
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/insert', [ProductController::class, 'insert']);
+Route::get('/products/update/{id}', [ProductController::class, 'update']);
+Route::get('/products/delete/{id}', [ProductController::class, 'delete']);
